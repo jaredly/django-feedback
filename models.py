b9af559376a5053b6b6186dfd77c7570e02dc4f4
Jared <jared@jared-laptop.(none)>
2010-02-27 16:41:19 -0700

    made ajax callback

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.sites.models import Site
 
 class Feedback(models.Model):
     site = models.ForeignKey(Site)
     url = models.CharField(max_length=255)
-    subject = models.CharField(max_length=255, blank=True)
+    subject = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
     text = models.TextField()
-    email = models.EmailField(blank=True)
 
     def __unicode__(self):
         return u'%s: %s' % (self.url, self.subject)
--- views.py
+++ views.py
@@ -1,6 +1,30 @@
 # Create your views here.
+from django.contrib.sites.models import Site
+from django.http import HttpResponse
+from django.utils.encoding import force_unicode
 
-def feedbackpost(request, url):
+import json
+import forms
+
+def sanitize(errors):
+    dct = dict((str(k),list(force_unicode(a) for a in v)) for k,v in errors.items())
+    return dct
+
+def handle_ajax(request, url):
     if not request.POST:
-        return render_to_response('feedback/basic_form.html',{})
+        return HttpResponse(json.dumps({'error':'no post recieved'}))
+    else:
+        post = {}
+        for k in request.POST:
+            post[k] = request.POST[k]
+        post['url'] = url
+        post['site'] = Site.objects.get_current().id
+        form = forms.FeedbackForm(post)
+        if form.is_valid():
+            form.save()
+            return HttpResponse(json.dumps({}))
+        else:
+            
+            return HttpResponse(json.dumps({'errors':sanitize(form.errors)}))
+
 

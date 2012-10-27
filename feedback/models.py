ecb43044467bab4c2d360ee53e74f7a7bca6f3c3
Unai Zalakain <contact@unaizalakain.info>
2012-10-28 00:04:37 +0200

    give translated verbose names to model fields

--- feedback/models.py
+++ feedback/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.contrib.sites.models import Site
+from django.utils.translation import ugettext as _
+
 
 class Feedback(models.Model):
-    site = models.ForeignKey(Site)
-    url = models.CharField(max_length=255)
-    subject = models.CharField(max_length=255, blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
-    text = models.TextField()
+    site = models.ForeignKey(Site, verbose_name=_('site'))
+    url = models.CharField(max_length=255, verbose_name=_('url'))
+    subject = models.CharField(max_length=255, blank=True, null=True,
+            verbose_name=_('subject'))
+    email = models.EmailField(blank=True, null=True, verbose_name=_('email'))
+    text = models.TextField(verbose_name=_('text'))
 
     def __unicode__(self):
         return u'%s: %s' % (self.url, self.subject)

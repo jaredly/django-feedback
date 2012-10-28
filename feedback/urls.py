45d20665744d98ff75b40b0a81333392521c8d94
Unai Zalakain <contact@unaizalakain.info>
2012-10-28 02:36:05 +0200

    syntax errors

--- feedback/urls.py
+++ feedback/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import patterns, url
 from feedback.views import FeedbackView
 
 urlpatterns = patterns('',
-        url(r'^ajax(?P<url>.*)$', FeedbackView.as_view(), name='feedback')
+        url(r'^ajax(?P<url>.*)$', FeedbackView.as_view(), name='feedback'),
     )
 
 # vim: et sw=4 sts=4
--- feedback/views.py
+++ feedback/views.py
@@ -16,7 +16,7 @@ class FeedbackView(CreateView):
 
     def get_form_kwargs(self):
         kwargs = super(FeedbackView, self).get_form_kwargs()
-        post = kwargs['data'].copy(
+        post = kwargs['data'].copy()
         post['url'] = self.kwargs['url']
         post['site'] = Site.objects.get_current().pk
         kwargs['data'] = post

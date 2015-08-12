6f075b8c8b5aec75b33b7e7245c10e27ccfd0d1f
Petr Dlouhý <petr.dlouhy@email.cz>
2015-08-12 17:13:27 +0200

    fix email send failure locale import

--- feedback/views.py
+++ feedback/views.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.contrib.sites.models import Site
 from django.core.mail import send_mail
+from django.utils.translation import ugettext as _
 
 import json
 

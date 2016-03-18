4c13ed83c858f32aa38fcb0f97f08eb4829e0fd4
Petr Dlouhý <petr.dlouhy@email.cz>
2016-03-18 13:39:39 +0100

    fix for Django 1.10

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from feedback.views import FeedbackView
 
-urlpatterns = patterns('',
+urlpatterns = [
         url(r'^ajax(?P<url>.*)$', FeedbackView.as_view(), name='feedback'),
-    )
+    ]
 
 # vim: et sw=4 sts=4
